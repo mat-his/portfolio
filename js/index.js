a1a0de98336b81f8a8ac6704f72925694f6d91f3
WDM-tech <wdm@dopy.tech>
2021-03-08 16:12:07 +0100

    My Linux Configuration

--- js/index.js
+++ js/index.js
@@ -4,13 +4,23 @@ function init() {
   console.log("this is init function");
 
   imageContainer = document.querySelector('.image-container');
+
+  var menu = document.getElementById("menu-hiding");
+
+  window.addEventListener('scroll', function(e) {
+    if (window.scrollY > 900) {
+      menu.style.visibility = "visible";
+    } else {
+      menu.style.visibility = "hidden";
+    }
+  });
 }
 
 var i = 0;
 
 function menu() {
   var menu_drawer = document.querySelector(".layout-tabs");
-  if (i%2==0) {
+  if (i%2===0) {
     menu_drawer.style.display = "inline";
     i++;
   } else {
